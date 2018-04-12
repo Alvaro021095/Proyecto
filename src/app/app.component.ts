fe376361ca0ff506cde4d6f06c0a5e031da15ec9
Alvaro <lotero021095@gmail.com>
2018-04-12 01:02:38 -0500

    CommitFinal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,46 +14,99 @@ import { Observable } from 'rxjs/Observable';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+	// Array de Tareas para  manejo de datos.
 	todos: Todo[];
+	// Array de Tareas Activas
+	actives: Todo[];
+	total: number;
+	item: string;
 	constructor(private service: TodoAppService) {
 		service.getTodos().subscribe((content) => {
 			this.todos = content;
+			this.actives = this.todos.filter((todo) => todo.status === 'Active');
+			this.total = this.actives.length;
+			if (this.total === 0) {
+				this.item = '';
+			} else if (this.total === 1) {
+				this.item = this.total + ' item left';
+			} else {
+				this.item = this.total + ' items left';
+			}
 		});
 	}
+	/**
+	 * Metodo que envia al servicio el nombre de la tarea
+	 * y así agregarla a la BD.
+	 * @param nameTwo Nombre de la tarea.
+	 */
 	addToTodos(nameTwo: HTMLInputElement) {
 		if (nameTwo.value !== '') {
 			this.service.addToTodos(nameTwo.value);
 			nameTwo.value = '';
 		}
 	}
-	changeToComplete(event, id: string, status: string) {
+	/**
+	 * Metodo que envia al servicio el id y el estado
+	 * de una tarea para que lo edite.
+	 * @param event Evento de check.
+	 * @param id Identificador de la tarea.
+	 * @param status Estado de la tarea a cambiar.
+	 */
+	changeToStatus(event, id: string, status: string) {
 		if (event.target.checked) {
-			this.service.changeToComplete(id, status);
+			this.service.changeToStatus(id, status);
+		} else {
+			this.service.changeToStatus(id, status);
 		}
 	}
+	/**
+	 * Metodo que permite enviar el id al servicio
+	 * y este lo eliminara.
+	 * @param id Identificador de la tarea.
+	 */
 	deleteTodo(id: string) {
 		this.service.deleteTodo(id);
 	}
+	/**
+	 * Metodo que permite filtrar al Array de tareas
+	 * y así obtener las tareas especificas dependiendo
+	 * del estado que se envie.
+	 * @param status Estado de la tarea.
+	 */
 	changeTable(status: string) {
 		this.todos = [];
-		console.log(this.todos);
+		this.service.getTodos().subscribe((content) => {
+			this.todos = content;
+			if (status === 'actives') {
+				this.todos = this.todos.filter((todo) => todo.status === 'Active');
+			} else if (status === 'completes') {
+				this.todos = this.todos.filter((todo) => todo.status === 'Complete');
+			} else if (status === 'showAll') {
+				this.service.getTodos();
+			}
+		});
+	}
+	/**
+	 * Metodo que emvia al servicio, un array de tareas
+	 * ya filtradas para que estas sean eliminadas.
+	 */
+	clearCompletes() {
 		this.service.getTodos().subscribe((content) => {
 			this.todos = content;
 		});
-		console.log(this.todos);
-		if (status === 'actives') {
-			this.todos = this.todos.filter((todo) => todo.status === 'Active');
-			console.log(this.todos);
-		} else if (status === 'completes') {
-			this.todos = this.todos.filter((todo) => todo.status === 'Complete');
-			console.log(this.todos);
-		} else if (status === 'showAll') {
-			console.log('todos');
-			this.service.getTodos();
-		}
+		this.todos = this.todos.filter((todo) => todo.status === 'Complete');
+		this.service.clearCompletes(this.todos);
 	}
-	clearCompletes(status: string) {
-		console.log(status);
-		this.service.clearCompletes(status);
+	/**
+	 * Metodo que permite enviar el array de tareas
+	 * para que este sea recorrido y asi cambiar el estado
+	 * de las tareas a "Complete" o a "Actives".
+	 */
+	changeToCompletes(event) {
+		if (event.target.checked) {
+			this.service.changeToCompletes(this.todos, 'Complete');
+		} else {
+			this.service.changeToCompletes(this.todos, 'Active');
+		}
 	}
 }
--- src/app/todo-app.service.ts
+++ src/app/todo-app.service.ts
@@ -5,9 +5,20 @@ import { AngularFirestore } from 'angularfire2/firestore';
 @Injectable()
 export class TodoAppService {
 	constructor(private db: AngularFirestore) {}
+	/**
+	 * Metodo que retorna un array con todos los datos de
+	 * la BD.
+	 */
 	getTodos() {
 		return this.db.collection<Todo>('Todos').valueChanges();
 	}
+	/**
+	 * Metodo que agrega una tarea en la BD enviando un id
+	 * que se genera con la funcion createId() y con el nombre
+	 * que llega por parametro y se le envia por defecto el
+	 * estado "Active".
+	 * @param name Nombre de la tarea.
+	 */
 	addToTodos(name: string) {
 		if (name !== '') {
 			const id = this.db.createId();
@@ -15,16 +26,44 @@ export class TodoAppService {
 			this.db.collection('Todos').doc(id).set({ id, name, status });
 		}
 	}
-	changeToComplete(id: string, status: string) {
+	/**
+	 * Metodo que busca en la BD la tarea con el id que
+	 * llego por parametro y cuando lo encuentra, le actualiza
+	 * el estado por el que estado que llega por parametro (status).
+	 * @param id Identificador de la tarea.
+	 * @param status Estado que se actualizara en la tarea.
+	 */
+	changeToStatus(id: string, status: string) {
 		this.db.collection('Todos').doc(id).update({ status });
 	}
+	/**
+	 * Metodo que elimina de la BD una tarea que tenga
+	 * el id que llega por parametro.
+	 * @param id Identificador de la tarea.
+	 */
 	deleteTodo(id: string) {
 		this.db.collection('Todos').doc(id).delete();
 	}
-	changeTable() {
-		return this.db.collection<Todo>('Todos').valueChanges();
+	/**
+	 * Metodo que recorre el array que llego por parametro, y a cada
+	 * uno lo elimina de la BD.
+	 * @param to Array de tareas que tienen el estado "Complete".
+	 */
+	clearCompletes(to: Todo[]) {
+		for (const todoo of to) {
+			this.db.collection('Todos').doc(todoo.id).delete();
+		}
 	}
-	clearCompletes(status: string) {
-		this.db.collection('Todos').doc(status).delete();
+	/**
+	 * Metodo que recorre el array que llega por parametro, y a cada
+	 * uno le actualiza el estado actual por el estado que llega por
+	 * parametro.
+	 * @param to Array de todas las tareas.
+	 * @param status Estado por el cual se actualizara.
+	 */
+	changeToCompletes(to: Todo[], status: string) {
+		for (const todoo of to) {
+			this.db.collection('Todos').doc(todoo.id).update({ status });
+		}
 	}
 }
